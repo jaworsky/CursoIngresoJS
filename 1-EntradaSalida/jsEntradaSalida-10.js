3488b6ebae9d8525bd06a292f812dd7270d43b9c
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-05 13:26:16 -0300

    10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,4 +5,22 @@ en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
 	
+var importe;
+var desc25delimporte;
+var resultado;
+
+importe=document.getElementById('importe').value;
+
+importe=parseInt(importe);
+
+desc25delimporte=(importe*25/100);
+
+resultado=(importe-desc25delimporte);
+
+alert("Resultado "+resultado);
+
+
+
+
+
 }
