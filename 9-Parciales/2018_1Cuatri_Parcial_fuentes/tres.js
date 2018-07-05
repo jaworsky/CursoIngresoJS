63fde0b7e8b0ebc3166d6105dba1afee1740a679
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-05 15:11:39 -0300

    1 2 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
 
+	var precio;
+	var porcentaje;
+	var preciocondescuento;
+	var preciofinal;
+
+
+	precio= prompt("ingrese el precio");
+	porcentaje=prompt("ingrese el porcentaje de descuento");
+
+	precio=parseInt(precio);
+	porcentaje=parseInt(porcentaje);
+	preciocondescuento=parseInt(preciocondescuento);
+
+	
+
+	//sacar porcentaje
+	preciocondescuento=(precio*porcentaje/100);
+
+	preciofinal=(precio-preciocondescuento);
+
+
+
+
+	// q se muestre en el cuadro, los datos deseados
+	document.getElementById('elPrecioFinal').value=preciofinal;
+	
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,21 @@
 function mostrar()
 {
 
+var ancho;
+var largo;
+var perimetro;
+
+ancho=prompt("ingrese el ancho");
+largo=prompt("ingrese el largo");
+
+ancho=parseInt(ancho);
+largo=parseInt(largo);
+
+perimetro=(ancho+largo)*2;
+
+alert("el perimetro es "+perimetro);
+
+
+
+
 }
