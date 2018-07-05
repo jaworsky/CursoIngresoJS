8dca25a5b1b555cd0e6e988b973285a5f7c3b833
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-05 12:56:24 -0300

    ej 7 funcionando

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -5,20 +5,75 @@ ej.: "la Resta es 750"*/
 function sumar()
 {	
 	
+	var numero1;
+	var numero2;
+	var resultado;
+
+	numero1=document.getElementById('numeroUno').value;
+	numero2=document.getElementById('numeroDos').value;
+
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+
+	resultado=(numero1+numero2);
+	alert("la suma es "+resultado);
+
+
 }
 
 function restar()
 {
 	
+	var numero1;
+	var numero2;
+	var resultado;
+
+	numero1=document.getElementById('numeroUno').value;
+	numero2=document.getElementById('numeroDos').value;
+
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+
+	resultado=(numero1-numero2);
+	alert("la resta es "+resultado);
+
+
 }
 
 function multiplicar()
 { 
-	
+	var numero1;
+	var numero2;
+	var resultado;
+
+	numero1=document.getElementById('numeroUno').value;
+	numero2=document.getElementById('numeroDos').value;
+
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+
+	resultado=(numero1*numero2);
+	alert("la multiplicación es "+resultado);
+
 }
 
 function dividir()
 {
 	
+
+var numero1;
+	var numero2;
+	var resultado;
+
+	numero1=document.getElementById('numeroUno').value;
+	numero2=document.getElementById('numeroDos').value;
+
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+
+	resultado=(numero1/numero2);
+	alert("la división es "+resultado);
+
+
 }
 
