d0c07af4c9104ebd44de54ac3b5803f921c9aed6
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-05 21:55:49 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,43 @@
 function mostrar()
 {
 
+	var precio;
+	var porcentajededescuento;
+	var descuento;
+	var preciocondescuento;
+	var iva;
+	var preciocondescuentomasiva;
+
+	
+	precio=prompt("ingrese el precio");
+	porcentajededescuento=prompt("ingrese el porcentaje de descuento");
+
+	precio=parseInt(precio);
+
+	descuento=(precio*porcentajededescuento)/100;
+	
+ 	alert=(descuento);
+
+	// 1
+
+	preciocondescuento=(precio-descuento);
+
+	alert(preciocondescuento);
+	// 2
+
+	iva=(precio*21)/100;
+
+	alert=(iva);
+
+	// 3
+
+	alert(descuento+ " "+ preciocondescuento+" " +iva);
+
+	//4
+
+	preciocondescuentomasiva=preciocondescuento+iva;
+
+
+	document.getElementById('elPrecioFinal').value=preciocondescuentomasiva;
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -2,4 +2,24 @@
 function mostrar()
 {
 
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base=prompt("ingrese la base");
+	altura=prompt("ingrese la altura");
+
+	base=parseInt(base);
+	altura=parseInt(altura);
+	
+	// no hace falta agregar parseInt a los resultados (perimetro y superficie)
+	
+	superficie=(base*altura/2);
+	perimetro=(base*3);
+
+	alert("la superficie es "+superficie+" el perimetro es "+perimetro);
+
+
+
 }
