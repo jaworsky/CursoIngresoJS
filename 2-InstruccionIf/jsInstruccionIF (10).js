9aecf293be18ba41bc724e34befdde26037c1d08
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-12 21:39:03 -0300

    terminados

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,28 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	
+	var notarandom;
+
+	notarandom=Math.floor(Math.random()*(11-1))+1;
+
+	if (notarandom>=9) {
+
+		alert("EXCELENTE");
+	}
+
+	else if (notarandom>=4) {
+
+		alert("APROBÓ");
+	}
+
+	else {  // el else no lleva condicion, el else if SI.
+
+		alert("Vamos, la proxima se puede");
+	}
+
+
+
+
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
 
-}//FIN DE LA FUNCIÓN
+	/* para poner numero random: 
+
+	var num;
+	numero=Math.Floor( Math.Random() * (MAX-MIN) ) + MIN;
+				redondea para abajo/     31-1      +   1; 
+	
+	*/
+
+	var numeroaleatorio;
+
+	numeroaleatorio=Math.floor(Math.random()*(11-1))+1;
+
+	alert(numeroaleatorio);
+
+
+
+
+}
