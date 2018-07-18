f7281c99126a2199aa50dc86f1cae26a9bbf7012
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-18 17:39:49 -0300

    ej terminado

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,19 +10,66 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
-var contadorIntentos;
+var numeroingresado;
+var contador;
+contador =0;
+
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
-	
+numeroSecreto=Math.floor(Math.random()*(101-1))+1;
+numeroSecreto=parseInt(numeroSecreto);
+//genero el numero secreto.
+alert(numeroSecreto);
 
 }
 
 function verificar()
 {
 	
-	
+numeroingresado=document.getElementById('numero').value;
+numeroingresado=parseInt(numeroingresado);
+contador=contador+1;
+
+
+if (numeroingresado==numeroSecreto && contador==1)
+{
+	alert("Usted es un Psíquico");
+}
+
+	else if (numeroingresado==numeroSecreto && contador==2)	
+	{
+	alert("Excelente percepción”");
+	}
+
+		else if (numeroingresado==numeroSecreto && contador==3)
+		{
+			alert("Esto es suerte");
+		}
+			else if (numeroingresado==numeroSecreto && contador==4)
+			{
+				alert("Excelente técnica");
+			}
+				else if (numeroingresado==numeroSecreto && contador==5)
+				{
+					alert("Usted está en la media");
+				}
+					else if (numeroingresado==numeroSecreto) // no puede haber un else if dentro de un else if
+					{
+						if (contador>=6 && contador<=10)
+						{
+							alert("falta técnica");
+						}
+						else
+						{
+							alert("afortunado en el amor");
+						}
+
+
+
+
+					}
 
 }
