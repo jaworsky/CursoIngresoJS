e16d85af0a7545900c78d507fb189f9749318a17
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-18 20:43:46 -0300

    ej 4

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,43 @@
 function mostrar()
 {
 
+var numero1;
+var numero2;
+var resultado;
+
+numero1=prompt("ingrese el numero 1");
+numero2=prompt("ingrese el numero 2");
+
+numero1=parseInt(numero1);
+numero2=parseInt(numero2);
+resultado=parseInt(resultado);
+
+
+if(numero1==numero2)
+{
+	alert(numero1+numero2);
+
+}
+else if(numero1>numero2)
+{
+	resultado=numero1-numero2;
+	alert(resultado);
+}
+else if(numero1<numero2)
+{
+	resultado=numero1+numero2;
+	alert(resultado);
+
+if (resultado>10)
+{
+	alert("La suma es "+resultado+" y se pasó de 10");
+}
+
+
+
+}
+
+
+
+
 }
