d105645df68c98e077d672d51a9cb8005b2a4fe4
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-19 16:45:24 -0300

    6

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,127 @@
 function mostrar()
 {
 
+var hora;
+
+hora=document.getElementById('laHora').value;
+hora=parseInt(hora);
+
+
+switch(hora)
+{
+
+	case 6:
+	alert("Es de mañana");
+	break;
+
+	case 7:
+	alert("Es de mañana");
+	break;
+
+	case 8:
+	alert("Es de mañana");
+	break;
+
+	case 9:
+	alert("Es de mañana");
+	break;
+
+	case 10:
+	alert("Es de mañana");
+	break;
+
+	case 11:
+	alert("Es de mañana");
+	break;
+
+
+	case 12:
+	alert("Es de tarde");
+	break;
+
+	case 13:
+	alert("Es de tarde");
+	break;
+
+	case 14:
+	alert("Es de tarde");
+	break;
+
+	case 15:
+	alert("Es de tarde");
+	break;
+
+	case 16:
+	alert("Es de tarde");
+	break;
+
+	case 17:
+	alert("Es de tarde");
+	break;
+
+	case 18:
+	alert("Es de tarde");
+	break;
+
+	case 19:
+	alert("Es de tarde");
+	break;
+
+	case 20:
+	alert("Es de noche");
+	break;
+
+	case 21:
+	alert("Es de noche");
+	break;
+
+	case 22:
+	alert("Es de noche");
+	break;
+
+	case 23:
+	alert("Es de noche");
+	break;
+
+	case 24:
+	alert("Es de noche");
+	break;
+
+
+	case 1:
+	alert("Es de noche");
+	alert("A dormir!");
+	break;
+
+	case 2:
+	alert("Es de noche");
+	alert("A dormir!")
+	break;
+
+	case 3:
+	alert("Es de noche");
+	alert("A dormir!")
+	break;
+
+	case 4:
+	alert("Es de noche");
+	alert("A dormir!")
+	break;
+
+	case 5:
+	alert("Es de noche");
+	alert("A dormir!")
+	break;
+
+	default :
+		
+	alert("La hora ingresada no es valida");
+
+
+}	
+
+
+
+
+
 }
