85e04ba6fa96102692ab4d5d1d6b47fd0c503ec3
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-19 16:22:54 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,57 @@
 function mostrar()
 {
 
+var planetasolar;
+
+planetasolar=prompt("ingrese el nombre de un planeta del sistema solar");
+
+
+switch(planetasolar)	// variables enteras o no
+{
+	case "tierra" :
+	
+		alert("Acá vivimos");
+		break;
+		
+	case "venus" : 		
+
+		alert("Acá hace mas calor");
+		break;
+
+	case "mercurio" :
+		alert("Acá hace mas calor");
+		break;
+
+	case "marte" :
+		alert("Acá hace mas frío");
+		break;
+
+	case "jupiter" :
+		alert("Acá hace mas frío");
+		break;
+
+	case "saturno" :
+		alert("Acá hace mas frío");
+		break;
+
+	case "urano" :
+		alert("Acá hace mas frío");
+		break;
+
+	case "neptuno" :
+		alert("Acá hace mas frío");
+		break;
+
+	case "pluton" :
+		alert("Acá hace mas frío");	
+		break;
+
+	default : 
+		alert("Aclaración: los planetas se ingresan en minúscula (Mercurio y Venus están antes que la tierra).");		
+
+}
+
+
+
+
 }
