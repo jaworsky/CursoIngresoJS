9508a84a2279a5b168e5bc3e91b49e9058b1c5f7
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-07-23 20:59:58 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,38 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+/* evalua una variable, swtich(variable) utiliza una igualdad estricta, compara los tipos de variables "===" despues los valores
+{
+	case 1:
+	codigo a ejectuar
+	break;
+	
+	default: como el else del if
+
+}
+
+*/
+
+switch (mesDelAño)
+{
+	case "Enero" :
+	alert("Que comiences bien el año!!!.");
+	break;
+
+	case "Marzo" :
+	alert("a clases");
+	break;
+
+	case"Julio":
+	alert("se vienen las vacaciones");
+	break;
+
+	case"Diciembre":
+	alert("Felices Fiestas");
+	break;
 
 
+}
 
-}//FIN DE LA FUNCIÓN
+
+} //FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -6,6 +6,41 @@ var mesDelAño = document.getElementById('mes').value;
 //alert (mesDelAño);
 	
 	
+switch(mesDelAño)
+{
+	case"Enero":
+	case"Marzo":
+	case"Mayo":
+	case"Julio":
+	case"Agosto":
+	case"Octubre":
+	case"Diciembre":
+	alert("si tiene 31 días.");
+	break;
+
+	case"Abril":
+	case"Junio":
+	case"Septiembre":
+	case"Noviembre":
+	alert("si tiene 30 días. ");
+	break;
+
+	default:
+
+	alert("si tiene 28 días. ");
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 
 
